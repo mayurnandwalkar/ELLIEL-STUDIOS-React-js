20b545ac8ee874ca184ada6f228e67cfca58ec3a
mayur <mayurnandwalkar0@gmail.com>
2023-05-03 01:17:03 +0530

    screening page done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './pages/Login';
 import Payment from './pages/Payment';
+import Screening from './pages/Screening';
 
 function App() {
   return (
     <>
       {/* <Login /> */}
-      <Payment/>
+      {/* <Payment/> */}
+      <Screening/>
     </>
   );
 }
